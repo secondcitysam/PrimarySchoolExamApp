b24158d11e8fe770501e95aa32606f1d897d47ab
secondcitysam <samyak.kedari4444@gmail.com>
2026-01-12 23:05:09 +0530

    f6g

---
+++ src/main/java/com/school/examportal/config/Solution.java
@@ -0,0 +1,40 @@
+package com.school.examportal.config;
+
+import java.util.List;
+
+public class Solution {
+
+    public ListNode reverserKGroup(ListNode head , int k)
+    {
+
+
+
+    }
+}
+
+class ListNode
+{
+
+    int data;
+    ListNode next;
+
+    ListNode()
+    {
+        data = 0;
+        next = null;
+    }
+
+    ListNode(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+
+    ListNode(int data,ListNode next)
+    {
+        this.data = data;
+
+        this.next = next;
+    }
+
+}
